058e22a6e5a4518f8fd0f81edbf3c976adac7819
Marilia Yovana <marilia_yovana@hotmail.com>
2019-06-06 01:57:40 -0500

    avance de html,javascript,css y cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,15 @@
 window.cipher = {
   encode: (offset, string) => {
-    /* Acá va tu código que cifra*/
+
+    let cifra ="";
+
+    for (let i=0; i<string.length; i++){
+    let asciNum = string[i].charCodeAt();
+    let ejecutar = ((asciNum-65) +offset) % 26 + 65
+    let letra = String.fromCharCode(ejecutar)
+    cifra += letra;
+  }
+    return cifra;/* Acá va tu código que cifra*/
   },
   decode: (offset, string) => {
     /* Acá va tu código que descifra*/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-const name = document.getElementById("usuario");
-const password = document.getElementById("contraseña");
-const texto = document.getElementById("textito");
-const numer = document.getElementById("numero");
 const resultado = document.getElementById("conclusion");
-const enviar = document.getElementById("btn1");
-const cifrar = document.getElementById("btn2");
-const decifrado = document.getElementById("btn3");
 const limpiar = document.getElementById("btn4");
 const iniciar = document.getElementById("btn5");
 
-const textName = ()=>{
+const primeraPantalla = document.getElementById("pantallaUno");
+const boton1 = document.getElementById("btn1");
+ let contador = 0;
+    boton1.addEventListener('click',() => {
+    contador++;
+const name = document.getElementById("usuario").value;
+    document.getElementById("usuario").value = "";
+console.log("nombre", name)
+const password = document.getElementById("contraseña").value;
+document.getElementById("contraseña").value = "";
+
+ if(password === "LABORATORIA"){
+    primeraPantalla.style.display = "none";
+    return  segundaPantalla.style.display = "block";   
+}else if(contador === 3){
+       primeraPantalla.style.display = "block";
+       alert('usuario denegado');
+}
+}); 
+
+const segundaPantalla = document.getElementById("pantallaDos");
+const cifrar = document.getElementById("btn2");
+cifrar.addEventListener('click',()=>{
+const texto = document.getElementById("texto").value;
+const numer = document.getElementById("numero").value;
+console.log("texto", texto);
+console.log("numero", numer);
+});
+
+const decifrado = document.getElementById("btn3");
+decifrado.addEventListener('click',()=>{ 
+const texto = document.getElementById("texto").value;
+const numer = document.getElementById("numero").value;
+console.log(texto);
+console.log(numer);
+});
 
 
-};
