fea80270adf7714883dffccb21e31d3a083e5a75
Marilia Yovana <marilia_yovana@hotmail.com>
2019-06-10 09:24:30 -0500

    la funcionalidade del boton limpiar e inicio

--- src/index.js
+++ src/index.js
@@ -1,55 +1,64 @@
 const primeraPantalla = document.getElementById("pantallaUno");
+const enviar = document.getElementById("enviar");
+const name = document.getElementById("usuario");
+const password = document.getElementById("contraseña");
 const segundaPantalla = document.getElementById("pantallaDos");
+const texto = document.getElementById("texto");
+const numer = document.getElementById("numero");
+const cifrar = document.getElementById("cifrar");
+const decifrado = document.getElementById("decifrar"); //boton cifrar//
+const error = document.getElementById("error");
 const resultado = document.getElementById("conclusion");
-const limpiar = document.getElementById("btn4");
-const iniciar = document.getElementById("btn5");
+const limpiar = document.getElementById("limpiar");
+const iniciar = document.getElementById("iniciar");
 
-//primera pantalla//
-const boton1 = document.getElementById("btn1");
 let contador = 0;
-boton1.addEventListener('click',() => {
+enviar.addEventListener('click',() => {
     contador++;
-    const name = document.getElementById("usuario").value;
-    document.getElementById("usuario").value = "";
-    console.log("nombre", name)
+    const nameUsuario = name.value;
+    const passwordControl = password.value;  
 
-    const password = document.getElementById("contraseña").value;
-    document.getElementById("contraseña").value = "";
-    if(password === "LABORATORIA"){
-    primeraPantalla.style.display = "none";
-  
-    return  segundaPantalla.style.display = "block";   
-    }else if(contador === 3){
-    primeraPantalla.style.display = "block";
-    const error = document.getElementById("error");
-    error.innerHTML = "tus intentos llegaron al limite";
-}
+    if(passwordControl === "LABORATORIA"){
+        primeraPantalla.classList.toggle("ocultar");
+        segundaPantalla.classList.toggle("ocultar"); 
+    }
+                 
+    else if(contador === 3){
+       error.innerHTML = "Ya utilizaste todos tus intentos, en este momento no podrás ingresar";
+    }
 }); 
 
-//segunda pantalla//
-
-const cifrar = document.getElementById("btn2");
+//segunda pantalla// 
 cifrar.addEventListener('click',()=>{
-const texto = document.getElementById("texto").value;
-const texCifrar = texto.toUpperCase();
-const numer = document.getElementById("numero");
-const offset = parseInt(numer.value);
- let cifrar = cipher.encode(offset,texCifrar);
-
-
-console.log( cifrar);
-
+   const texCifrar = texto.value.toUpperCase();
+   const offset = parseInt(numer.value);
+   let cifrar = cipher.encode(offset,texCifrar);
+   resultado.value = cifrar;
 });
-
-const decifrado = document.getElementById("btn3");
 decifrado.addEventListener('click',()=>{ 
-const texto = document.getElementById("texto").value;
-const texCifrar = texto.toUpperCase();
-const numer = document.getElementById("numero");
-const offset = parseInt(numer.value);
-let cifrar = cipher.decode(offset,texCifrar);
-
-console.log(cifrar);
+    const textoDecifrar = texto.value;
+    const texCifrar = textoDecifrar.toUpperCase();
+    const offset = parseInt(numer.value);
+    let cifrar = cipher.decode(offset,texCifrar);
+    resultado.value =cifrar;
 });
+limpiar.addEventListener('click', () => {
+    texto.value ="";
+    numer.value ="";
+    resultado.value ="";
+    cifrar.value ="";
+    decifrado.value ="";
+
+});
+
+iniciar.addEventListener('click',() =>{
+    primeraPantalla.classList.toggle("ocultar");
+    segundaPantalla.classList.toggle("ocultar"); 
+             
+             
+});
+ 
+
+
 
 
