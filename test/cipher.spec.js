22aa2bc22556a855c1e4f8b394bc5c8cf350b04d
Marilia Yovana <marilia_yovana@hotmail.com>
2019-06-17 11:38:38 -0500

    decifrar y cifrar de ñ

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -19,8 +19,14 @@ describe('cipher', () => {
     it('debería retornar "-./0123456" para "0123456789"  con offset 30',() => {
       assert.equal(cipher.encode(30,"0123456789"),"-./0123456");
     });
-    it('debería retornar "ñ" para "ñ"  con offset 33',() => {
-      assert.equal(cipher.encode(33,"ñ"),"ñ");
+    it('debería retornar "ÒÓÔÕÖ×ØÙÚÍÎÏÐÑ" para "ÍÎÏÐÑÒÓÔÕÖ×ØÙÚ"  con offset 33',() => {
+      assert.equal(cipher.encode(33,"ÍÎÏÐÑÒÓÔÕÖ×ØÙÚ"),"ÒÓÔÕÖ×ØÙÚÍÎÏÐÑ");
+    });
+    it('debería retornar "ëìíîïðñòóôõöèéê" para "èéêëìíîïðñòóôõö"  con offset 33',() => {
+      assert.equal(cipher.encode(33,"èéêëìíîïðñòóôõö"),"ëìíîïðñòóôõöèéê");
+    });
+    it('debería retornar "ç" para "ç"  con offset 33',() => {
+      assert.equal(cipher.encode(33,"ç"),"ç");
     });
   });
 
@@ -38,6 +44,12 @@ describe('cipher', () => {
     });
     it('debería retornar "/012345678" para "-./0123456" con offset 30',() => {
       assert.equal(cipher.decode(30,"-./0123456"),"/012345678");
+    });
+    it('debería retornar "ÍÎÏÐÑÒÓÔÕÖ×ØÙÚ" para "ÒÓÔÕÖ×ØÙÚÍÎÏÐÑ" con offset 30',() => {
+      assert.equal(cipher.decode(30,"ÒÓÔÕÖ×ØÙÚÍÎÏÐÑ"),"ÍÎÏÐÑÒÓÔÕÖ×ØÙÚ");
+    });
+    it('debería retornar "ëìíîïðñòóôõöèéê" para "èéêëìíîïðñòóôõö" con offset 33',() => {
+      assert.equal(cipher.decode(33,"èéêëìíîïðñòóôõö"),"ëìíîïðñòóôõöèéê");
     });
     it('debería retornar " " para " " con offset 33',() => {
       assert.equal(cipher.decode(33," ")," ");
